e643943a637276d65153a6da2078a5de4c71af6c
unknown <delvikarmilasari@gmail.com>
2023-06-13 22:44:44 +0800

    Melengkapi Data Mahasiswa

--- src/universitas/mahasiswa.java
+++ src/universitas/mahasiswa.java
@@ -5,9 +5,40 @@ package universitas;
 public class mahasiswa {
     String nama, prodi, npm;
    
-    public String getDetail() {
+public String getDetail() {
     return nama+npm+prodi;
+} 
+ public mahasiswa (String nama, String prodi, String npm){
+     this.nama=nama;
+     this.npm=npm;
+     this.prodi=prodi;
+ 
+ }    
+
+    public String getNama() {
+        return nama;
     }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getProdi() {
+        return prodi;
+    }
+
+    public void setProdi(String prodi) {
+        this.prodi = prodi;
+    }
+
+    public String getNpm() {
+        return npm;
+    }
+
+    public void setNpm(String npm) {
+        this.npm = npm;
+    }
+ 
 
     
 }
